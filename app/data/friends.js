2f789be6c623d5970cb13921e147beaf78dcfec0
Javier Avitia <Javier.A.Avitia@gmail.com>
2017-08-24 23:26:17 -0700

    Added more users.

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,6 +11,34 @@ var friends = [{
     q8: "weak",
     q9: "offended",
     q10: "broke"
+},
+{
+    name: "UsuarioUno",
+    image: "https://pbs.twimg.com/media/Co3jneoUkAEGml1.jpg",
+    q1: "pizza",
+    q2: "dog",
+    q3: "sometimes",
+    q4: "bernie",
+    q5: "fascista",
+    q6: "neither",
+    q7: "games",
+    q8: "morning",
+    q9: "neutral",
+    q10: "cities"
+},
+{
+    name: "UsuarioDos",
+    image: "https://s-media-cache-ak0.pinimg.com/originals/a5/43/7e/a5437ecc2ab152de7c0d69ed800af4a4.gif",
+    q1: "noodles",
+    q2: "cat",
+    q3: "often",
+    q4: "yes",
+    q5: "dead",
+    q6: "neither",
+    q7: "books",
+    q8: "neither",
+    q9: "bad",
+    q10: "international"
 }];
 
 module.exports = friends;
